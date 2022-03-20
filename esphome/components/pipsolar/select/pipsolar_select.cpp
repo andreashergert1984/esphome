439da17b514c7c9c6fbabb018e9ab6b4cba70eb4
Andreas <andreas.hergert@otrs.com>
2022-03-20 23:04:20 +0100

    save

--- esphome/components/pipsolar/select/pipsolar_select.cpp
+++ esphome/components/pipsolar/select/pipsolar_select.cpp
@@ -31,7 +31,7 @@ void PipsolarSelect::map_and_publish(std::string &value){
   ESP_LOGD(TAG, "got value: %s",value.c_str());
   if (this->status_mapping_.find(value) != this->status_mapping_.end()) {
     ESP_LOGD(TAG, "found mapped option %s for option %s",this->status_mapping_[value].c_str(),value.c_str());
-    this->publish_state(value);
+    this->publish_state(this->status_mapping_[value]);
   } else {
     ESP_LOGD(TAG,"could not find option %s in mapping",value.c_str());
     return;
